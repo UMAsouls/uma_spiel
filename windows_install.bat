d2288161d0673750c48084a050b092231f179b8e
UMAsouls <umaplay972@gmail.com>
2026-04-13 02:04:34 +0900

    [add] windows用インストールbat

---
+++ windows_install.bat
@@ -0,0 +1,6 @@
+git clone --single-branch --depth 1 https://github.com/pybind/pybind11.git pybind11
+git clone --single-branch --depth 1 https://github.com/pybind/pybind11_json.git open_spiel\pybind11_json
+git clone --single-branch --depth 1 https://github.com/abseil/abseil-cpp.git open_spiel\abseil-cpp
+git clone https://github.com/pybind/pybind11_abseil.git open_spiel\pybind11_abseil
+git clone -b develop --single-branch --depth 1 https://github.com/jblespiau/dds.git open_spiel\games\bridge\double_dummy_solver
+git clone --single-branch --depth 1 https://github.com/nlohmann/json.git open_spiel\json
